c92ce47c0793b2947c410088a6a6690c9871ecb8
joshsamara <joshsamara@gmail.com>
2013-11-21 07:07:42 -0500

    added upgrading of parts

--- load.py
+++ load.py
@@ -10,11 +10,10 @@ class LoadingBar(object):
         #levels of properties
         self.loadingText = 0
         self.finishedText = 0
-        self.addedText = 0
+        self.introText = 0
         self.brackets = 0
         self.barStyle = 0
         self.counter = 0
-        self.message = 0
 
         #Dynamic counter
         self.current = 0
@@ -22,23 +21,58 @@ class LoadingBar(object):
         #Static max
         self.max = 10
 
-
-    def getMessage(self):
-        messages = {
+    def getLoadingText(self):
+        texts = {
         0: "2 load pres spcae alot",
         2: "Press space to load...",
         3: "Loading: Press Space to process faster."}
-        return messages[self.message]
+        return texts[self.loadingText]
 
     def isLoaded(self):
         return self.current >= self.max
 
     def load(self):
-        print self.getMessage()
+        print self.getLoadingText()
         while not self.isLoaded():
             if getInput() == " ":
                 self.current += 1
         print "Done"
+
+    def upgrade(self):
+        prettyDict = {
+        "loadingText": "Loading Text",
+        "finishedText": "Completion Text",
+        "introText": "Introduction Text",
+        "brackets": "Bracket Style",
+        "barStyle": "Loading Bar Style",
+        "counter": "Completion Counter"}
+
+        upgradeable = {}
+        i = 0
+        for key in self.__dict__:
+            if key in prettyDict and self.__dict__[key] < 3:
+                upgradeable[i] = key
+                i += 1
+
+        if len(upgradeable) == 0:
+            print "Fully upgraded"
+            print "Press any key to continue..."
+            getInput()
+        else:
+            print "Press the matching key to upgrade:"
+            maxVal = len(upgradeable)
+            for i in range(maxVal):
+                place = "%s) " % i
+                key = upgradeable[i]
+                name  = "%-25s" % prettyDict[key]
+                level = "Level: %d" % self.__dict__[key]
+                print place + name + level
+            upChoice = getNum(maxVal)
+            key = upgradeable[upChoice]
+            self.__dict__[key] += 1
+            print "\nUpgraded %s" % prettyDict[key]
+            print "Press any key to continue..."
+            getInput()
 
 
 def clear():
@@ -51,10 +85,23 @@ def getInput():
     else:
         # clear()
         print ""
-        printStats()
         sys.exit()
 
+def getNum(maxVal = 9):
+    userInput = ""
+    valid = False
+    while not valid:
+        try:
+            userInput = int(userInput)
+            valid = userInput <= maxVal
+        except:
+            userInput = getInput()   
+    return userInput
 
 clear()
 a = LoadingBar()
 a.load()
+a.upgrade()
+a.upgrade()
+a.upgrade()
+
